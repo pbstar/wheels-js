2d3a73466e2870ede1411e7bf54a049dd132269c
pengbo <pengbo@hopefound.cn>
2022-12-12 15:16:32 +0800

    excel

--- src/package/exportExcel/index.js
+++ src/package/exportExcel/index.js
@@ -1,39 +1,26 @@
-function toExcel(FileName, ShowLabel, JSONData, classObj) {
-  //先转化json
-  let classTypeText = ''
-  if (classObj.jobCode == 1) {
-    classTypeText = 'GYB'
-  } else if (classObj.jobCode == 2) {
-    classTypeText = 'SYB'
+function toExcel(objInfo) {
+  let obj = {
+    fileName: objInfo.fileName || '文件名',
+    FootName: objInfo.FootName || 'sheet1',
+    theadList: objInfo.theadList || [],
+    tbodyList: objInfo.tbodyList || []
   }
-  var arrData = typeof JSONData != 'object' ? JSON.parse(JSONData) : JSONData;
-  var excel = '<table>';
-  //设置表头
-  var row = "<tr align='center'><th colspan='4'>" + classTypeText + "创业培训结业评分册</th>";//设置Excel的左居中
-  //换行
-  excel += row + "</tr>";
-  row = "<tr align='center'><td colspan='4'>模块: " + classTypeText + " 班次：" + classObj.trainingShift + " 起止时间：" + classObj.startStudyDate.slice(0, 10) + "至" + classObj.endStudyDate.slice(0, 10) + "</td>";//设置Excel的左居中
-  //换行
-  excel += row + "</tr>";
-  row = "<tr align='left'>";//设置Excel的左居中
-  for (var i = 0, l = ShowLabel.length; i < l; i++) {
-    for (var key in ShowLabel[i]) {
-      row += "<td>" + ShowLabel[i][key] + '</td>';
+  let excel = '<table style="font-size:15px;font-family:Microsoft YaHei">';
+  for (let i = 0; i < obj.theadList.length; i++) {
+    excel += "<tr align='center'>"
+    let th = obj.theadList[i]
+    for (let h = 0; h < th.length; h++) {
+      excel += "<th colspan=" + th[h].colspan + " rowspan=" + th[h].rowspan + ">" + th[h].text + "</th>"
     }
+    excel += "</tr>";
   }
-  //换行
-  excel += row + "</tr>";
-  //设置数据
-  for (var i = 0; i < arrData.length; i++) {
-    var rowData = "<tr align='left'>";
-    for (var y = 0; y < ShowLabel.length; y++) {
-      for (var k in ShowLabel[y]) {
-        if (ShowLabel[y].hasOwnProperty(k)) {
-          rowData += "<td style='vnd.ms-excel.numberformat:@'>" + (arrData[i][k] === null ? "" : arrData[i][k]) + "</td>";
-        }
-      }
+  for (let i = 0; i < obj.tbodyList.length; i++) {
+    excel += "<tr align='center'>"
+    let td = obj.tbodyList[i]
+    for (let d = 0; d < td.length; d++) {
+      excel += "<td colspan=" + td[d].colspan + " rowspan=" + td[d].rowspan + ">" + td[d].text + "</td>"
     }
-    excel += rowData + "</tr>";
+    excel += "</tr>";
   }
   excel += "</table>";
   var excelFile = "<html xmlns:o='urn:schemas-microsoft-com:office:office' xmlns:x='urn:schemas-microsoft-com:office:excel' xmlns='http://www.w3.org/TR/REC-html40'>";
@@ -47,7 +34,7 @@ function toExcel(FileName, ShowLabel, JSONData, classObj) {
   excelFile += "<x:ExcelWorksheets>";
   excelFile += "<x:ExcelWorksheet>";
   excelFile += "<x:Name>";
-  excelFile += FileName;
+  excelFile += obj.FootName;
   excelFile += "</x:Name>";
   excelFile += "<x:WorksheetOptions>";
   excelFile += "<x:DisplayGridlines/>";
@@ -62,11 +49,11 @@ function toExcel(FileName, ShowLabel, JSONData, classObj) {
   excelFile += excel;
   excelFile += "</body>";
   excelFile += "</html>";
-  var uri = 'data:application/vnd.ms-excel;charset=utf-8,' + encodeURIComponent(excelFile);
+  var url = 'data:application/vnd.ms-excel;charset=utf-8,' + encodeURIComponent(excelFile);
   var link = document.createElement("a");
-  link.href = uri;
+  link.href = url;
   link.style = "visibility:hidden";
-  link.download = FileName + '评分册' + ".xls";
+  link.download = obj.fileName + ".xlsx";
   document.body.appendChild(link);
   link.click();
   document.body.removeChild(link);
--- src/views/doc/exportExcel/index.vue
+++ src/views/doc/exportExcel/index.vue
@@ -1,39 +1,131 @@
 <template>
   <div class="doc">
-    <h2>PageFooter</h2>
-    <p class="p">本节将介绍如何在项目中使用 Zvie。</p>
-    <h3>引入 Zvie</h3>
-    <p class="p">
-      你可以引入整个
-      Zvie，暂不支持根据需要仅引入部分组件。我们先介绍如何引入完整的 Zvie。
-    </p>
-    <p class="p">在 main.js 中写入以下内容：</p>
+    <h2>ExportExcel 导出Excel</h2>
+    <p class="p">验证码生成器。</p>
+    <fieldset>
+      <legend>示例用法</legend>
+    </fieldset>
+    <el-button @click="toE">导出</el-button>
     <div class="code">
-      <p>
-        import Vue from "vue"; <br />
-        import App from "./App.vue"; <br />
-        import zvie from "zvie"; <br />
-        import "zvie/zvie.css"; <br />
-        Vue.config.productionTip = false;<br />
-        Vue.use(zvie); <br />
-        new Vue({ <br />
-        &nbsp;&nbsp;render: (h) => h(App),<br />
-        }).$mount("#app");
-      </p>
+      <pre>
+        &lt;zvi-verification @updateCode="updateCode">&lt;/zvi-verification>
+      </pre>
     </div>
-    <p class="p">
-      以上代码便完成了 Zvie 的引入。需要注意的是，样式文件需要单独引入。
-    </p>
-    <h3>开始使用</h3>
-    <p class="p">
-      至此，一个基于 Vue 和 Zvie
-      的开发环境已经搭建完毕，现在就可以编写代码了。各个组件的使用方法请参阅它们各自的文档。
-    </p>
+    <fieldset>
+      <legend>属性列表</legend>
+    </fieldset>
+    <table>
+      <tr>
+        <th>属性</th>
+        <th>说明</th>
+        <th>类型</th>
+        <th>可选值</th>
+        <th>默认值</th>
+      </tr>
+      <tr>
+        <td>contentWidth</td>
+        <td>验证码容器宽度</td>
+        <td>number</td>
+        <td>—</td>
+        <td>100</td>
+      </tr>
+      <tr>
+        <td>contentHeight</td>
+        <td>验证码容器高度</td>
+        <td>number</td>
+        <td>—</td>
+        <td>44</td>
+      </tr>
+      <tr>
+        <td>identifyCodes</td>
+        <td>验证码取值来源</td>
+        <td>string</td>
+        <td>—</td>
+        <td>1234567890ABCDEFGHIJKLMNOPQRSTUVWXYZ</td>
+      </tr>
+      <tr>
+        <td>identifyNumber</td>
+        <td>验证码取值数量</td>
+        <td>number</td>
+        <td>—</td>
+        <td>4</td>
+      </tr>
+      <tr>
+        <td>fontSizeMin</td>
+        <td>验证码字体最小值</td>
+        <td>number</td>
+        <td>—</td>
+        <td>25</td>
+      </tr>
+      <tr>
+        <td>fontSizeMax</td>
+        <td>验证码字体最大值</td>
+        <td>number</td>
+        <td>—</td>
+        <td>35</td>
+      </tr>
+      <tr>
+        <td>bgcMin</td>
+        <td>验证码图片背景色最小值</td>
+        <td>number</td>
+        <td>—</td>
+        <td>200</td>
+      </tr>
+      <tr>
+        <td>bgcMax</td>
+        <td>验证码图片背景色最大值</td>
+        <td>number</td>
+        <td>—</td>
+        <td>220</td>
+      </tr>
+    </table>
+    <fieldset>
+      <legend>事件列表</legend>
+    </fieldset>
+    <table>
+      <tr>
+        <th>事件</th>
+        <th>说明</th>
+        <th>回调参数</th>
+      </tr>
+      <tr>
+        <td>updateCode</td>
+        <td>验证码改变</td>
+        <td>验证码</td>
+      </tr>
+    </table>
   </div>
 </template>
 <script>
+import obj from '@/package/exportExcel'
 export default {
   name: "docPageFooter",
+  data() {
+    return {
+      list: []
+    }
+  },
+  created() {
+    // this.toE()
+  },
+  methods: {
+    toE() {
+      let thlist = [
+        [{ text: '测试表格表头', colspan: '4' }],
+        [{ text: '学员姓名' }, { text: '手机号' }, { text: '理论考试成绩' }, { text: '作业成绩' }]
+      ]
+      let tdlist = []
+      for (let i = 0; i < 5; i++) {
+        tdlist.push([{ text: '姓名' + (i + 1) }, { text: '13322223333' }, { text: '23' }, { text: '56' }])
+      }
+      let objInfo = {
+        fileName: '文件名',
+        theadList: thlist,
+        tbodyList: tdlist
+      }
+      obj.toExcel(objInfo)
+    }
+  }
 };
 </script>
 
